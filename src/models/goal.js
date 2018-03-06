e333134701d11518c8346fe8cda137b4103c0b14
Shannon <schristinep@gmail.com>
2018-03-06 13:45:49 -0600

    goal methods write and retrieve goals from db

--- src/models/goal.js
+++ src/models/goal.js
@@ -4,4 +4,48 @@ var Goal = {};
 
 // TODO: ADD MORE MODEL FUNCTIONS HERE
 
+Goal.addGoal = function(title, description, user) {
+	return db('goals').insert({
+		title: title,
+		description: description,
+		user: user
+	})
+	.then(function(goal) {
+		console.log('goal added!');
+	})
+	.catch(function(err) {
+		console.log('error adding goal :-(')
+	})
+};
+
+Goal.retrieveById = function(id) {
+	return db('goals').where({ user: id }).select('*')
+		.then(function(goal) {
+			console.log('Goal retrieved: ', goal);
+		})
+		.catch(function(err) {
+			console.log('error retrieving goal :-(')
+		})
+};
+
+Goal.markGoalCompleted = function(id) {
+	return db('goals').where({ id: id }).update({ completed: true })
+		.then(function(goal) {
+			console.log('goal completed!');
+		})
+		.catch(function(err) {
+			console.log('failed to mark goal completed :-(');
+		})
+};
+
+Goal.findGoalsByUserId = function(userid) {
+	return db('goals').where({ user: userid }).select('*')
+		.then(function(goals) {
+			console.log('Users goals: ', goals);
+		})
+		.catch(function(err) {
+			console.log('failed to retrieve users goals :-(')
+		})
+};
+
 module.exports = Goal;
