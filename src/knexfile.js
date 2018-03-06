1e90f303aa2dac7ddfe7286c07f9553f1ce633e1
Shannon <schristinep@gmail.com>
2018-03-06 11:51:51 -0600

    knexfile defines schemas

--- src/knexfile.js
+++ src/knexfile.js
@@ -2,6 +2,11 @@ module.exports = {
 
   development: {
     // TODO: ADD CONNECTION INFO
-  }
+    client: 'sqlite3',
+    connection: { 
+    	filename: './db.sqlite3'
+    }
+  },
+  //production: { client: 'pg', connection: process.env.DATABASE_URL }
 
 };
--- src/migrations/20170111171601_create_tables.js
+++ src/migrations/20170111171601_create_tables.js
@@ -1,15 +1,24 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTableIfNotExists('users', function (table) {
-    table.integer('id');
+    table.increments();
     // TODO: DESCRIBE THE USER TABLE
+    table.string('username').defaultTo(null);
+    table.string('password').defaultTo(null);
   });
 
   // TODO: CREATE ANY OTHER TABLES YOU NEED
+  knex.schema.createTableIfNotExists('goals', function(table) {
+  	table.increments();
+  	table.string('title').defaultTo(null);
+  	table.string('description').defaultTo(null);
+  	table.integer('user').references('id').inTable('users').defaultTo(null);
+  })
 };
 
 exports.down = function(knex, Promise) {
   // TODO: DROP OTHER TABLES
 
   return knex.schema.dropTable('users');
+  knex.schema.dropTable('goals');
 
 };
