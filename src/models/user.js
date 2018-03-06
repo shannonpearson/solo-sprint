ab61e921e09f21295f8e72ba5a2a259c3535f97e
Shannon <schristinep@gmail.com>
2018-03-06 13:20:39 -0600

    queries database to insert user with username and password

--- src/models/user.js
+++ src/models/user.js
@@ -3,15 +3,28 @@ var db = require('./db');
 var User = {};
 
 User.findByUsername = function(username) {
-  return db('users').where({ username: username }).select('*')
-    .then(function(user) {
-      return user;
-    })
-    .catch(function(err) {
-      console.error(err)
-    });
+    return db('users').where({ username: username }).select('*')
+	    .then(function(user) {
+	        return user;
+	    })
+	    .catch(function(err) {
+	        console.error(err)
+	    });
 };
 
 // TODO: ADD MORE MODEL FUNCTIONS HERE
+
+User.addUser = function(username, password) {
+	return db('users').insert({ username: username, password: password })
+	    .then(function(user) {
+		  	res.json({ 
+		  		success: true, 
+		  		message: 'successfully inserted user into database'
+		  	})
+	    })
+	    .catch(function(err) {
+	    	console.log('error inserting user into database');
+	    });
+}
 
 module.exports = User;
